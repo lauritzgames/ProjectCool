d397b47211ce66360398b92594b4d0c558642daa
lause <90100237+lausestorm@users.noreply.github.com>
2025-04-29 18:15:15 +0200

    dsa

--- script.js
+++ script.js
@@ -6,3 +6,39 @@ window.addEventListener('scroll', () => {
         button.style.display = 'none';
     }
 });
+
+var count = 2;
+
+function validate ()
+{
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["admin"]
+   var passwordArray = ["4FEiHzqD2iAzGEv!_HG.Gk"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+if (user == usernameArray[i])
+if (password == passwordArray [i])
+{
+   valid = true;
+   break;
+}
+
+if(valid)
+{
+   alert("Login was successful");
+   window.location = "dsjaij983wej9d9823jd8934hrf438923h2df98wehf823rhfwe98fhw98e9f8hsfhdsiofuh9382.html";
+   return false ;
+}
+var again = "tries";
+if (count ==1)
+{
+   again = "try"
+}
+if(count >= 1)
+{
+   alert("Wrong password or username")
+   count--;
+}
+}
