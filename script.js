0ba4ea7ca9392b3f7ae8dd8ce7303e76ffc469e9
lause <90100237+lausestorm@users.noreply.github.com>
2025-04-29 17:26:11 +0200

    sadas

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const button = document.querySelector('.back-to-top');
+    if (window.scrollY > 200) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+});
